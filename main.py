05ab61a550570436d91ea7cbf5f4c866ebd51b8f
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-06-04 13:01:57 +0200

    Working snake game

--- main.py
+++ main.py
@@ -7,37 +7,41 @@ import neat
 WIN_WIDTH = 500
 WIN_HEIGHT = 500
 
-class SnakePart:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
 class Snake:
     def __init__(self):
-        self.x = 25
-        self.y = 25
-        self.parts = [SnakePart(25, 25), SnakePart(24, 25), SnakePart(23, 25)]
+        self.x = 250
+        self.y = 250
 
-def draw_window(win):
+
+def draw_window(win, snake):
     pygame.display.update()
+
 
 def main():
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     fps = pygame.time.Clock()
     snake = Snake()
+    snake_position = [250, 250]
+    snake_body = [[250, 250],
+                  [240, 250],
+                  [230, 250],
+                  [220, 250]
+                  ]
+    score = 0
 
     direction = 'RIGHT'
     change_to = direction
-    snake_position = SnakePart(snake.x, snake.y)
-    fruit_location = [random.randrange(1, 50),
-                      random.randrange(1, 50)]
+    fruit_location = [random.randrange(1, 50) * 10,
+                      random.randrange(1, 50) * 10]
     fruit_spawn = True
+    frame = 0
 
     run = True
     while run:
+        frame = frame + 1
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                print(event.key)
                 if event.key == pygame.K_UP:
                     change_to = 'UP'
                 if event.key == pygame.K_DOWN:
@@ -61,25 +65,37 @@ def main():
             direction = 'RIGHT'
 
         if direction == 'UP':
-            snake_position.y -= 1
+            snake_position[1] -= 10
         if direction == 'DOWN':
-            snake_position.y += 1
+            snake_position[1] += 10
         if direction == 'LEFT':
-            snake_position.x -= 1
+            snake_position[0] -= 10
         if direction == 'RIGHT':
-            snake_position.x += 1
+            snake_position[0] += 10
 
-        snake.parts.append(snake_position)
+        snake_body.insert(0, list(snake_position))
+        if snake_position[0] == fruit_location[0] and snake_position[1] == fruit_location[1]:
+            score += 10
+            fruit_spawn = False
+        else:
+            snake_body.pop()
+
+        if not fruit_spawn:
+            fruit_location = [random.randrange(1, 50) * 10,
+                      random.randrange(1, 50) * 10]
+
+        fruit_spawn = True
+        win.fill((0, 0, 0))
+        for pos in snake_body:
+            pygame.draw.rect(win, (0,255,0),
+                             pygame.Rect(pos[0], pos[1], 10, 10))
 
         if fruit_spawn:
-            pygame.draw.rect(win, (255, 10, 10),
-                             pygame.Rect(fruit_location[0] * 10, fruit_location[1] * 10, 10, 10))
+            pygame.draw.rect(win, (255, 0, 0),
+                             pygame.Rect(fruit_location[0], fruit_location[1], 10, 10))
 
-        for part in snake.parts:
-            pygame.draw.rect(win, (0, 255, 0),
-                pygame.Rect(part.x * 10, part.y * 10, 10, 10))
-        draw_window(win)
-        fps.tick(2)
+        draw_window(win, snake)
+        fps.tick(10)
 
 
 main()
